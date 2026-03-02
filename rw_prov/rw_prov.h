6f8c843a7051f3f0b15e7ae16bb9cc0f9b782c90
jared1467321 <12duncajar@gmail.com>
2026-03-02 07:22:25 -0700

    Update rw_prov.h

--- rw_prov/rw_prov.h
+++ rw_prov/rw_prov.h
@@ -1,17 +1,20 @@
+#ifndef RW_PROV_H
+#define RW_PROV_H
+
 #include <stdint.h>
 #include <stddef.h>
 
-typedef uint64_t addr_t;
-
-#ifdef __cplusplus 
-extern "C" { 
+#ifdef __cplusplus
+extern "C" {
 #endif
 
-//read kernel
+typedef uint64_t addr_t;
+
+// Read kernel
 uint32_t kernel_read32(uint64_t);
 uint64_t kernel_read64(uint64_t);
 
-//write kernel
+// Write kernel
 void kernel_write32(uint64_t, uint32_t);
 void kernel_write64(uint64_t, uint64_t);
 
@@ -20,9 +23,11 @@ void kernel_write(uint64_t where, const void *buf, size_t size);
 
 void kernel_getbase(uint64_t *retkbase, uint64_t *retkslide);
 
-// 0 - success, other - implementation related
-int rw_prov_init();
+// 0 = success, otherwise implementation-defined
+int rw_prov_init(void);
 
-#ifdef __cplusplus 
+#ifdef __cplusplus
 }
+#endif
+
 #endif
